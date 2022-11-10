4749ae36bf60405bafdbb6ec968f2dd63f8ceff4
khangtranbc123 <{t.v.khang202@gmail.com}>
2022-11-10 09:49:50 +0700

    reset

--- backEnd/VMG/src/main/java/com/example/vmg/respository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.vmg.respository;
-
-import com.example.vmg.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department,Long> {
-}
