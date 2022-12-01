fc818a7f843b179c033b8deae3c2b54db097d873
phongdhhe151364 <phongdhhe151364@fpt.edu.vn>
2022-12-01 11:25:19 +0700

    dfxg

--- frontEnd/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import TheHeader from "./layout/TheHeader.vue";
-import TheContent from "./layout/TheContent.vue";
-export default {
-  install(Vue) {
-    Vue.component("TheHeader", TheHeader);
-    Vue.component("TheContent", TheContent);
-  },
-};
